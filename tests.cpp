acbd7a242c5d1dc1917cf619b106073a35cad8c2
El Mahdi Abouraouf <eabourao@e1r12p3.1337.ma>
2025-12-07 17:43:27 +0100

    asd

--- tests.cpp
+++ tests.cpp
@@ -1,34 +1,65 @@
-// save as test.cpp
+#include <string.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
-#include <arpa/inet.h>
 #include <iostream>
-#include <cstring>
+    #include <unistd.h>
+#define MYPORT "3490"  // the port users will be connecting to
+#define BACKLOG 10     // how many pending connections queue holds
 
-int main()
+int main(void)
 {
+    struct sockaddr_storage their_addr;
+    socklen_t addr_size;
     struct addrinfo hints, *res;
+    int sockfd, new_fd;
 
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family   = AF_INET;      // IPv4
-    hints.ai_socktype = SOCK_STREAM;  // TCP
-    hints.ai_flags    = AI_PASSIVE;   // server mode
+    // !! don't forget your error checking for these calls !!
 
-    int status = getaddrinfo(NULL, "6667", &hints, &res);
-    if (status != 0) {
-        std::cerr << "getaddrinfo error: " << gai_strerror(status) << std::endl;
-        return 1;
+    // first, load up address structs with getaddrinfo():
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;  // use IPv4 or IPv6, whichever
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
+
+    getaddrinfo(NULL, "1234", &hints, &res);
+
+    // make a socket, bind it, and listen on it:
+
+    sockfd = socket(res->ai_family, res->ai_socktype,
+                                                 res->ai_protocol);
+    bind(sockfd, res->ai_addr, res->ai_addrlen);
+    listen(sockfd, BACKLOG);
+
+    // now accept an incoming connection:
+
+    addr_size = sizeof their_addr;
+    while (1)
+    {
+    new_fd = accept(sockfd, (struct sockaddr *)&their_addr,
+                                                       &addr_size);
+    if (new_fd >= 0)
+        std::cout << "connected suuccesfully" << std::endl;
+    else
+        break ;
+    int length = send(new_fd, "asd", 4, 0);
+    std::cout << length << std::endl;
+    char buf[1024];
+        while (1) {
+            int n = recv(new_fd, buf, sizeof(buf)-1, 0);  
+
+            if (n <= 0) {                     
+                if (n < 0) perror("recv");
+                else printf("Client closed connection\n");
+                break;
+            }
+
+            buf[n] = '\0';                    // null-terminate
+            printf("Received: %s", buf);      // see it on server terminal
+            send(new_fd, "You wrote: ", 11, 0);
+            send(new_fd, buf, n, 0);
+        }
+    close(new_fd);
     }
-
-    struct sockaddr_in *ipv4 = (struct sockaddr_in *)res->ai_addr;
-    char ip_str[INET_ADDRSTRLEN];
-
-    inet_ntop(AF_INET, &ipv4->sin_addr, ip_str, INET_ADDRSTRLEN);
-
-    std::cout << "Ready to bind to:\n";
-    std::cout << "  IP   : " << ip_str << "  (0.0.0.0 = all interfaces)\n";
-    std::cout << "  Port : " << ntohs(ipv4->sin_port) << "\n";
-    int s = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-    freeaddrinfo(res);
-    return 0;
 }
